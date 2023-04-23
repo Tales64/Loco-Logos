37e31407da5c82083341b19e10d43426732f5a72
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-04-23 03:04:53 -0700

    added switch statement & final render information

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const questions = [
     {
         type: "input",
         name: "text",
-        message: "TEXT: Enter up to (3) Characters:",
+        message: "Text: Enter up to (3) Characters:",
     },
     {
         type: "input",
         name: "text-color",
-        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "Text Color: Enter a color keyword (OR a hexadecimal number):",
     },
     {
         type: "list",
@@ -22,6 +22,27 @@ const questions = [
     {
         type: "input",
         name: "shape-color",
-        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "Shape Color: Enter a color keyword (OR a hexadecimal number):",
     },
 ];
+
+function generateLogo(text, textColor, shape, shapeColor) {
+    let shapeImage;
+    switch (shape) {
+      case 'Circle':
+        shapeImage = new Circle();
+        break;
+      case 'Square':
+        shapeImage = new Square();
+        break;
+      case 'Triangle':
+        shapeImage = new Triangle();
+        break;
+    }
+    shapeImage.colorChoice(shapeColor)
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    <rect x="70" y="20" width="160" height="160" fill="black"/>
+      ${shapeImage.render()}
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${text-color}" font-size="70">${text}</text>
+    </svg>`;
+  }

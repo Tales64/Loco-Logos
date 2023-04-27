40fe469a46b38f748e91663c4c6a1e7ba6eb10f0
Darryl Tillman <113705249+Tales64@users.noreply.github.com>
2023-04-26 18:09:52 -0700

    fixed some test and render problems

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const { Circle, Square, Triangle } = require('./lib/shapes');
+const  {Circle, Square, Triangle} = require ('./lib/shapes.js');
 
 const questions = [
     {
@@ -10,7 +10,7 @@ const questions = [
     },
     {
         type: "input",
-        name: "text-color",
+        name: "textColor",
         message: "Text Color: Enter a color keyword (OR a hexadecimal number):",
     },
     {
@@ -21,12 +21,24 @@ const questions = [
     },
     {
         type: "input",
-        name: "shape-color",
+        name: "shapeColor",
         message: "Shape Color: Enter a color keyword (OR a hexadecimal number):",
     },
 ];
 
-function generateLogo(text, textColor, shape, shapeColor) {
+inquirer.prompt(questions).then(answers => {
+    const { text, textColor, shape, shapeColor } = answers;
+    const logo = createLogo(text, textColor, shape, shapeColor);
+    fs.writeFile('./sampleLogo.svg', logo, err => {
+      if (err) {
+        console.log('Error writing file:', err);
+      } else {
+        console.log('Generated sampleLogo.svg!');
+      }
+    });
+  });
+
+function createLogo(text, textColor, shape, shapeColor) {
     let shapeImage;
     switch (shape) {
       case 'Circle':
@@ -39,10 +51,10 @@ function generateLogo(text, textColor, shape, shapeColor) {
         shapeImage = new Triangle();
         break;
     }
-    shapeImage.colorChoice(shapeColor)
+    shapeImage.setColor(shapeColor)
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     <rect x="70" y="20" width="160" height="160" fill="black"/>
       ${shapeImage.render()}
-      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${text-color}" font-size="70">${text}</text>
+      <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${textColor}" font-size="70">${text}</text>
     </svg>`;
   }
